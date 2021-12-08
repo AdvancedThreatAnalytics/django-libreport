747e1b34eb73b816c012d9b53a081d4e69a50eed
Kostiuk Michael <10185391+michael-kostiuk@users.noreply.github.com>
2021-12-08 09:05:15 +0200

    Improve Report Schedule list view in django admin (#36)
    * Improve Report Schedule list view in django admin
    
    * Improve report admin and add re-generate action

--- reports/admin.py
+++ reports/admin.py
@@ -1,13 +1,58 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.db.models import Q
+
 from .models import Report, ReportSchedule
+from .tasks import generate_document
 
 
 @admin.register(Report)
 class ReportAdmin(admin.ModelAdmin):
-    list_display = ('id', 'report', 'typ', 'created_by', 'deleted')
-    list_filter = ('organization', 'deleted')
+    actions = ("regenerate_report",)
+    list_display = (
+        "id",
+        "report",
+        "typ",
+        "created_by",
+    )
+    list_filter = ("organization",)
+
+    def regenerate_report(self, request, queryset):
+        """
+        From the given queryset: only regenerate the reports which have an empty
+        document, to avoid overwriting reports with valid documents generated.
+        """
+        regenerated_count = 0
+        requested_count = queryset.count()
+
+        for report in queryset.filter(Q(document="") | Q(document=None)):
+            generate_document.apply_async(args=(report.id,), countdown=30)
+            regenerated_count += 1
+
+        msg = "Re-generating {} reports out of {}".format(
+            regenerated_count, requested_count
+        )
+
+        if regenerated_count < requested_count:
+            self.message_user(request, msg, messages.ERROR)
+        else:
+            self.message_user(request, msg, messages.SUCCESS)
+
+    regenerate_report.short_description = "Re-generate selected Reports"
 
 
 @admin.register(ReportSchedule)
 class ReportScheduleAdmin(admin.ModelAdmin):
-    pass
+    list_display = (
+        "id",
+        "report",
+        "name",
+        "typ",
+        "created_by",
+    )
+    list_filter = ("organization", "period")
+    search_fields = (
+        "name",
+        "created_by__name",
+        "created_by__email",
+        "organization__psa_id",
+    )
