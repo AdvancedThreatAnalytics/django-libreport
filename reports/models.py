ab01a512cc72c637c131a1509761cb672ccba374
Priyanka <priyanka.sharma@criticalstart.com>
2024-09-10 18:49:06 -0500

    CERB-3961 - Update BaseReportModel to add a new field for timezone

--- reports/models.py
+++ reports/models.py
@@ -68,6 +68,7 @@ class BaseReportModel(models.Model):
     config = models.JSONField(blank=True, default=dict)
     emails = ArrayField(models.EmailField(max_length=255), blank=True, null=True)
     deleted = models.BooleanField(default=False)
+    report_timezone = models.CharField(max_length=64, required=False, blank=True, null=True)
 
     class Meta:
         abstract = True
