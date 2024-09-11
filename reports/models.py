a5464487c64d09b9b1c3b9d4ddfa4efb308a26fe
Priyanka <priyanka.sharma@criticalstart.com>
2024-09-10 19:47:23 -0500

    Update formatting

--- reports/models.py
+++ reports/models.py
@@ -68,7 +68,9 @@ class BaseReportModel(models.Model):
     config = models.JSONField(blank=True, default=dict)
     emails = ArrayField(models.EmailField(max_length=255), blank=True, null=True)
     deleted = models.BooleanField(default=False)
-    report_timezone = models.CharField(max_length=64, required=False, blank=True, null=True)
+    report_timezone = models.CharField(
+        max_length=64, required=False, blank=True, null=True
+    )
 
     class Meta:
         abstract = True
