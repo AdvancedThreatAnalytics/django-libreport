c20cba6e7cc4ee0bc7525329de6be98658c0467c
Kevin Olson <110241893+keo-cs@users.noreply.github.com>
2025-08-01 15:16:51 -0500

    GRPH-2577 - Add acks_late & idempotency to generate_report task (#61)
    * GRPH-2577 - Add acks_late & idempotency to generate_report task
    
    * black formatting

--- reports/tests/models_report.py
+++ reports/tests/models_report.py
@@ -1,8 +1,9 @@
 from datetime import datetime
-from django.test import TestCase
 
+from django.test import TestCase
 from reports.models import Report
 from reports.runtests.example.models import Organization
+from reports.tasks import generate_document
 
 
 class ReportModelTestCase(TestCase):
@@ -24,3 +25,31 @@ class ReportModelTestCase(TestCase):
         self.assertEqual(report.document.read(), b"Some data")
         file_name = "org-example-report-2017-01-01-to-2017-01-02.pdf"
         self.assertTrue(report.document.name.endswith(file_name))
+
+    def test_generate_document_idempotency(self):
+        from unittest.mock import patch
+
+        start = datetime(2017, 1, 1, 12, 33)
+        end = datetime(2017, 1, 2, 12, 33)
+        org = Organization.objects.create(name="Org2")
+        report = Report.objects.create(
+            report="example",
+            organization=org,
+            start_datetime=start,
+            end_datetime=end,
+            typ="pdf",
+        )
+
+        with patch.object(
+            Report, "generate_document", wraps=report.generate_document
+        ) as mock_generate:
+            generate_document.apply(args=(report.pk,))
+            report.refresh_from_db()
+            self.assertEqual(report.status, Report.STATUS_COMPLETED)
+            self.assertEqual(mock_generate.call_count, 1)
+
+            # Report is already COMPLETED, so it should not be generated again
+            generate_document.apply(args=(report.pk,))
+            report.refresh_from_db()
+            self.assertEqual(report.status, Report.STATUS_COMPLETED)
+            self.assertEqual(mock_generate.call_count, 1)
